424f171f8051866ec0430872a6792363fa132950
Jeroen Claassens <support@favware.tech>
2023-03-11 09:50:26 +0100

    feat: setup skeleton

---
+++ .prettierrc.cjs
@@ -0,0 +1,22 @@
+const sapphirePrettierConfig = require('@sapphire/prettier-config');
+
+module.exports = {
+	...sapphirePrettierConfig,
+	overrides: [
+		{
+			files: '*.svg',
+			options: {
+				parser: 'xml'
+			}
+		},
+		{
+			files: ['README.md', 'apps/**/*.md'],
+			options: {
+				tabWidth: 2,
+				useTabs: false,
+				printWidth: 120,
+				proseWrap: 'always'
+			}
+		}
+	]
+};
